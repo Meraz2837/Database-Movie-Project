5bca8260becbf571ec287bf67f3922364b9a342d
Mazbaur Rashid Meraz <mazbaurrashidmeraz@gmail.com>
2021-08-04 23:41:44 +0600

    up

--- ShowMovieGenreTable.php
+++ ShowMovieGenreTable.php
@@ -26,3 +26,17 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="Style.css">
+  <title>Show Movie Genre</title>
+</head>
+<body class="text-white">
+  
+</body>
+</html>
