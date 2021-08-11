0e670aa4f51ca91a501275a3d90b66333f38192a
Mazbaur Rashid Meraz <mazbaurrashidmeraz@gmail.com>
2021-08-11 23:00:08 +0600

    Update AddTableOptions.php

--- AddTableOptions.php
+++ AddTableOptions.php
@@ -104,7 +104,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 <nav style="background-color: rgba(0, 0, 0, 0.2) !important;" class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="Homepage.php"><img class="img-fluid icon" src="Icons/Copy of Movie.png" alt=""></a>
+      <a class="navbar-brand" href="index.php"><img class="img-fluid icon" src="Icons/Copy of Movie.png" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
